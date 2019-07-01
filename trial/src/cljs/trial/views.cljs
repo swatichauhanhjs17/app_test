4e3f9842a1860e709be0db7c72a35f4c1f1c3891
swatichauhanhjs17 <49249293+swatichauhanhjs17@users.noreply.github.com>
2019-07-01 19:00:45 +0530

    Update views.cljs

--- trial/src/cljs/trial/views.cljs
+++ trial/src/cljs/trial/views.cljs
@@ -3,9 +3,16 @@
    [re-frame.core :as re-frame]
    [trial.subs :as subs]
    ))
+(defn simple-component []
+  [:div
+   [:p "I am a component!"]
+   [:p.someclass
+    "I have " [:strong "bold"]
+    [:span {:style {:color "red"}} " and red "] "text."]])
 
 (defn main-panel []
   (let [name (re-frame/subscribe [::subs/name])]
     [:div
      [:h1 "Hello from " @name]
+     [simple-component]
      ]))
