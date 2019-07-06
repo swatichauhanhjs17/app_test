ad8a6840e2a024499e2ca0d7da37c3d304915125
swatichauhanhjs17 <49249293+swatichauhanhjs17@users.noreply.github.com>
2019-07-06 13:18:55 +0530

    Update core.cljs

--- trial/src/cljs/trial/core.cljs
+++ trial/src/cljs/trial/core.cljs
@@ -15,7 +15,7 @@
 
 (defn mount-root []
   (re-frame/clear-subscription-cache!)
-  (reagent/render [views/main-panel]
+  (reagent/render [views/music-component]
                   (.getElementById js/document "app")))
 
 (defn ^:export init []
